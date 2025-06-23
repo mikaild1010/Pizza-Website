bf837baa24a09886bd0fd03fafe2adbedb56f511
Mikail Demir <m.demir@smf.de>
2025-06-23 19:31:10 +0200

    first commit

---
+++ src/helpers/MenuList.tsx
@@ -0,0 +1,44 @@
+import Pepperoni from "../assets/images/pepperoni.jpg";
+import Margherita from "../assets/images/margherita.jpg";
+import PedroTechSpecial from "../assets/images/pedrotechspecial.jpg";
+import Vegan from "../assets/images/vegan.jpg";
+import Pineapple from "../assets/images/pineapple.jpg";
+import Expensive from "../assets/images/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 12.99,
+  },
+
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 8.99,
+  },
+
+  {
+    name: "PedroTechSpecial Pizza",
+    image: PedroTechSpecial,
+    price: 14.99,
+  },
+
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 13.99,
+  },
+
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 12.99,
+  },
+
+  {
+    name: "Expensive Pizza",
+    image: Expensive,
+    price: 17.99,
+  },
+];
---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/images/pizza.jpeg";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+        <h1> Pedro's Pizzeria</h1>
+        <p> PIZZA TO FIT ANY TASTE</p>
+        <Link to="/Menu">
+          <button> ORDER NOW</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
