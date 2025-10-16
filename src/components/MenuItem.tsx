4ffc816879f54ecadc338eb6fec419ceddf4bcac
Martin Oleschuk <m.oleschuk@smf.de>
2025-10-16 12:24:49 +0200

    Removed bun, Fixed deploy error

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,4 +1,6 @@
-function MenuItem({ image, name, price }) {
+type MenuItemProps = { image: string; name: string; price: number };
+
+function MenuItem({ image, name, price }: MenuItemProps) {
   return (
     <div className="menuItem">
       <div style={{ backgroundImage: `url(${image})` }}></div>
